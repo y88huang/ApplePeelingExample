f721ffce8b5bfe5e6766837cb9b3aaffe89f4895
Yundi Huang <kenkaka.hyd@gmail.com>
2015-04-16 16:58:32 -0400

    Initial Commit

---
+++ ApplePeelingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePeelingExample
+//
+//  Created by Ken Huang on 2015-04-16.
+//  Copyright (c) 2015 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePeelingExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePeelingExample
+//
+//  Created by Ken Huang on 2015-04-16.
+//  Copyright (c) 2015 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
